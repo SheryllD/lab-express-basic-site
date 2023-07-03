5b361e14664ee0fa17af4f8436b690d675c66faa
Sheryll Dumapal <sheryllhhs@outlook.com>
2023-07-04 00:48:49 +0200

    save

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
 const express = require('express'); 
+
+//we create our own server named app in here 
+// Express server will be handeling requests and responses
 const app = express();
 
+app.use(express.static('public'));
+
+app.get('/', (request, response) => {
+    response.send('<h1> My root route </h1>')
+}); 
 
 app.listen(3000, () => {
     console.log("server is listening on port 3000");
 }); 
+
+app.get('/home', (request, response) => {
+    response.sendFile(__dirname + '/views/index.html')
+}); 
+
+app.get('/about', (request, response) => {
+    response.sendFile(__dirname + '/views/about.html')
+}); 
+
+app.get('/contact', (request, response) => {
+    response.sendFile(__dirname + '/views/contact.html')
+}); 
+app.get('/work', (request, response) => {
+    response.sendFile(__dirname + '/views/work.html')
+}); 
+
+app.listen(3000, () => console.log('My first app listening on port 3000!'))
